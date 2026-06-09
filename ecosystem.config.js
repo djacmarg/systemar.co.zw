e5592c49458df0b8ec5a9bdb7e370651b2126e14
Tech Pundit <durotoye.oyerinde@gmail.com>
2026-06-09 15:15:13 +0200

    changed ecosystem.config.js to sz

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "SYSTEMAR-CO-ZW-APP",
-      cwd: "/var/www/systemar.co.zw/client",
+      cwd: "/var/www/systemar.co.zw",
       script: "npm",
       args: "run start",
       env: {
